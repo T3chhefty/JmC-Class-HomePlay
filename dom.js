babd8beb079bcebd315e0b85a416189deb6b0a10
T3chhefty <shadrachndu85@gmail.com>
2024-02-21 14:04:47 +0100

    adjusted the js file arrangement and added vscode folder

--- dom.js
+++ dom.js
@@ -8,8 +8,6 @@ const addBlockBtn = document.querySelector("button:first-child");
 const RemoveMDWGC = document.querySelector("button:nth-child(2)");
 const RemoveChildWGC = document.querySelector("button:last-child");
 
-//
-
 // this code was given
 addBlockBtn.addEventListener("click", addBlock);
 
@@ -53,25 +51,22 @@ function addBlock() {
 }
 // scrapping section
 
- 
 //Remove mother div with a given color
 RemoveMDWGC.addEventListener("click", () => {
-
-   const userColor = prompt("Enter Color");
-   mainColor = userColor.toLowerCase();
+  const userColor = prompt("Enter Color");
+  mainColor = userColor.toLowerCase();
   //  console.log(mainColor);
   // return all the children section
   // scarpe section
- const all = section.querySelectorAll("div");
+  const all = section.querySelectorAll("div");
   all.forEach((item) => {
-    if (item.innerHTML.length = 1) {
-      if(item.style.backgroundColor == mainColor){
-        item.remove()
+    if ((item.innerHTML.length = 1)) {
+      if (item.style.backgroundColor == mainColor) {
+        item.remove();
       }
     }
   });
 });
-
 
 //remove child with a given color
 RemoveChildWGC.addEventListener("click", () => {
@@ -79,45 +74,27 @@ RemoveChildWGC.addEventListener("click", () => {
   mainColor = userColor.toLowerCase();
   // console.log(mainColor);
   // scarpe section
- const all = section.querySelectorAll("div");
+  const all = section.querySelectorAll("div");
   all.forEach((item) => {
     if (item.innerHTML.length < 1) {
-     if (item.style.backgroundColor == mainColor) {
-       item.remove();
-       
-     }
+      if (item.style.backgroundColor == mainColor) {
+        item.remove();
+      }
     }
   });
-
-//
 
 });
 
 /*
 HOMEPLAY
 Run the code above, Study and understand the code block above both the html and js file and complete the following:
-
-
 Write a code that:
 1) the "Add Block" button adds a new block inside the section using the addBlock function already provided.
-
 2) when you click the "change color button", it changes the colour of its mother div and its sister div respectively to any random color of either RED, ORANAGE, YELLOW, GREEN, BLUE, INDIGO and VIOLET
-
-
 3) The color of the mother div and the color of the child div must never be the same)
-
-////////////////////////////////////////////////////////////////
 4) If you click the child div or block, only the colour of its mother div changes
-
-
 5) If you click the mother div, the colour of all the child divs or block in all the mothers div becomes the same
-
-
 6) Write a function for the "Remove a Mother div with a given colour" button that removes any mother with a specified colour from the DOM
-
-
 7) Write a function for the "Remove a child div with a given colour" button that removes any mother div from the DOM whose child div has a specified colour from the DOM
-
-
 NOTE AGAIN: DO NOT WRITE ANYTHING IN THE HTML DOCUMENT OR ADJUST WHAT HAS BEEN WRITTEN ALREADY IN THE JS FILE, JUST ADD MORE CODE TO THE JS FILE ONLY AND EVERYTHING YOU DO MUST BE IN THE JS FILE ALONE
 */
